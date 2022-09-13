43610c75d59ebee14ef6bef6ccbf65604ade7ee7
efezino idisi <enduranceidisi13@gmail.com>
2022-09-14 00:37:21 +0100

    add solution to task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of addition
+ */
+
+int add(int a, int b)
+{
+	int re;
+
+	re = a + b;
+	return (re);
+}
