6ade1fd9c33458f8ea04d4a1c0af2f891e74166a
efezino idisi <enduranceidisi13@gmail.com>
2022-09-19 23:08:58 +0100

    add task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ *
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int b, len;
+	char p;
+
+	len = 0;
+	while (*(s + len) != '\0')
+		len++;
+	len--;
+	for (b = 0; b < len; b++)
+	{
+		p = *(s + len);
+		*(s + len) = *(s + b);
+		*(s + b) = p;
+		len--;
+	}
+}
