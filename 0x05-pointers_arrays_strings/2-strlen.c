6c3e106720b9629ef20fca8f530af07b78e4fc58
efezino idisi <enduranceidisi13@gmail.com>
2022-09-19 18:31:03 +0100

    add task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <string.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be checked
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
