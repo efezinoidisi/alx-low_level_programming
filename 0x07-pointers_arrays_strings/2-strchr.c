8d6171a65774c4fa0bdfc3dec89e214b05e137a7
efezino idisi <enduranceidisi13@gmail.com>
2022-09-27 13:04:03 +0100

    final debug for task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,10 +10,10 @@ char *_strchr(char *s, char c)
 {
 	int a;
 
-	for (a = 0; *(s + a) != '\0'; a++)
+	for (a = 0; *s; a++)
 	{
 		if (*(s + a) == c)
 			return (s + a);
 	}
-	return ((char *)0);
+	return ('\0');
 }
