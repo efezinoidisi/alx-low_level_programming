72ee7a160e035f87fd5479d902ce3bb8e391f18e
efezino idisi <enduranceidisi13@gmail.com>
2022-09-27 01:10:09 +0100

    add task 8

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
+void set_string(char **s, char *to);
 
 
 
