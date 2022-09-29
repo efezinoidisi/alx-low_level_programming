eb28035ff51619be2406dc9ce5f0d60369641b37
efezino idisi <enduranceidisi13@gmail.com>
2022-09-29 18:06:58 +0100

    add task 5

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,8 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int is_prime_number(int n);
+int _sqrt_recursion(int n);
+int get_sqrt(int n, int c);
 
 
 #endif
