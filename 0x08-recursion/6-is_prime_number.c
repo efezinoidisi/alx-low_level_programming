695366d0120984b2592019343a76a958f7425c7e
efezino idisi <enduranceidisi13@gmail.com>
2022-09-29 18:13:25 +0100

    update task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * is_prime_number - check if number is a prime number
  * @n: number
@@ -7,8 +8,6 @@
 
 int is_prime_number(int n)
 {
-	int check_prime(int n, int a);
-
 	if (n <= 1)
 		return (0);
 	else if (n == 2)
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,6 +10,7 @@ int _pow_recursion(int x, int y);
 int is_prime_number(int n);
 int _sqrt_recursion(int n);
 int get_sqrt(int n, int c);
+int check_prime(int n, int a);
 
 
 #endif
