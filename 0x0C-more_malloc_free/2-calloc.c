e782109f55d2917f4039ad0bbe9eca0d013d6dfc
efezino idisi <enduranceidisi13@gmail.com>
2022-10-06 14:13:14 +0100

    add task 3 solution

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of elements
+ * @size: size of each bytes
+ *
+ * Return: pointer to memory or NULL
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *mem;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	mem = malloc(nmemb * size);
+	if (mem == NULL)
+		return (NULL);
+	return (mem);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,7 @@
 void *malloc_checked(unsigned int b);
 unsigned int _strlen(char *str);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 
 #endif
