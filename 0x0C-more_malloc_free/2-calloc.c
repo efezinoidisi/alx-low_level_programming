e5060553adc8784a9b31492473fd4e1432bd60ca
efezino idisi <enduranceidisi13@gmail.com>
2022-10-06 18:01:17 +0100

    update4 task 2 solution

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *mem = {0};
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 
 	mem = malloc(nmemb * size);
