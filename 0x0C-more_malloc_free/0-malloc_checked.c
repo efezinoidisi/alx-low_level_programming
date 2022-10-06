82d0543ba03e92e06d4910aeb7f72d6b4473191a
efezino idisi <enduranceidisi13@gmail.com>
2022-10-06 12:45:54 +0100

    fix betty error task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,8 +11,8 @@ void *malloc_checked(unsigned int b)
 {
 	void *mem;
 
-	mem = malloc(sizeof mem * b);
+	mem = malloc(sizeof(mem) * b);
 	if (mem == NULL)
-		exit (98);
+		exit(98);
 	return (mem);
 }
