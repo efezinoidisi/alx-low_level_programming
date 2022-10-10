3a9fc559ec9d081aa8e425c8b47e8748eac7d2f0
efezino idisi <enduranceidisi13@gmail.com>
2022-10-10 18:48:21 +0100

    add task 5 solution

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs
+ * @d: dog variable
+ *
+ * Return: Nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+		free(d);
+}
