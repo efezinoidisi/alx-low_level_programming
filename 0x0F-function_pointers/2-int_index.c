a08e39c2b6c10d5212883ab5c41626419121a1fe
efezino idisi <enduranceidisi13@gmail.com>
2022-10-12 14:18:48 +0100

    add task 2 solution

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+/**
+ * int_index - searches for an integer
+ * @array: array of integers
+ * @size: size of the array
+ * @cmp: pointer to function
+ *
+ * Return: index of integer or -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (size <= 0)
+		return (-1);
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index]) != 0)
+			return (index);
+	}
+	return (-1);
+}
