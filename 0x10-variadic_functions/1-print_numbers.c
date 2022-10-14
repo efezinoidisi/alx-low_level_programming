f8aeedafa35b65ad6e10808c033e9ed1c9a57bf4
Idisi Efezino Endurance <enduranceidisi13@gmail.com>
2022-10-14 10:02:40 -0700

    update exit status

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list numbers;
 
 	if (n == 0)
-		exit(98);
+		exit(0);
 	va_start(numbers, n);
 	for (i = 0; i < n; i++)
 	{
