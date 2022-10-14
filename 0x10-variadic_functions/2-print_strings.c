da68bdb6ed228541080a910670b0c1e8917ec74a
Idisi Efezino Endurance <enduranceidisi13@gmail.com>
2022-10-14 10:14:31 -0700

    update code

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,8 +16,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list strings;
 	char *current_string;
 
-	if (n == 0)
-		return;
 	va_start(strings, n);
 	for (i = 0; i < n; i++)
 	{
