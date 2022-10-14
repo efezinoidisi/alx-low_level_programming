a2a8753e6602adfa6a75281bd14cde60a7162dad
Idisi Efezino Endurance <enduranceidisi13@gmail.com>
2022-10-14 10:12:22 -0700

    remove a condition

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,8 +15,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list numbers;
 
-	if (n == 0)
-		exit(0);
 	va_start(numbers, n);
 	for (i = 0; i < n; i++)
 	{
