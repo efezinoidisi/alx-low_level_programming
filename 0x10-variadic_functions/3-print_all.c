bc2aef6e43e4a1faff00b117c5621aa155264b33
Idisi Efezino Endurance <enduranceidisi13@gmail.com>
2022-10-15 10:03:43 -0700

    fixed some checker errors

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,49 +1,43 @@
 #include <stdio.h>
 #include <stdarg.h>
-
 /**
  * print_all - prints anything
  * @format: format string
- *
  * Return: Nothing
  */
-
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int i = 0, j = 0;
+	unsigned int i = 0, p;
 	char *str;
 
 	va_start(args, format);
 	while (format[i] != '\0')
 	{
+		p = 0;
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(args, int));
-				j++;
+				p = printf("%c", va_arg(args, int));
 				break;
 			case 'i':
-				printf("%d", va_arg(args, int));
-				j++;
+				p = printf("%d", va_arg(args, int));
 				break;
 			case 'f':
-				printf("%f", va_arg(args, double));
-				j++;
+				p = printf("%f", va_arg(args, double));
 				break;
 			case 's':
 				str = va_arg(args, char *);
 
 				if (str == NULL)
 				{
-					printf("(nil)");
+					p = printf("(nil)");
 					break;
 				}
-				printf("%s", str);
-				j++;
+				p = printf("%s", str);
 				break;
 		}
-		if (format[i + 1] != '\0' && i == j)
+		if ((format[i + 1] != '\0') && (p != 0))
 			printf(", ");
 		i++;
 	}
