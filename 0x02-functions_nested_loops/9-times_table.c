cf3721b70b08d7ae7b1e899d3443db68ea1da8cb
efezino idisi <enduranceidisi13@gmail.com>
2022-10-16 22:31:12 +0100

    complete task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,25 +19,21 @@ void times_table(void)
 			c = a * b;
 			if (c >= 0 && c <= 9)
 			{
+				if (b != 0 && b != 10)
+				{
+					_putchar(' ');
+				}
 				_putchar(c + '0');
 			}
 			else
 			{
-				int d, e;
-
-				d = c % 10;
-				e = c / 10;
-				_putchar(e + '0');
-				_putchar(d + '0');
+				_putchar(c / 10 + '0');
+				_putchar(c % 10 + '0');
 			}
 			if (a != 10 && b != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
-				if (c / 10 == 0)
-				{
-					_putchar(' ');
-				}
 			}
 		}
 		_putchar('\n');
