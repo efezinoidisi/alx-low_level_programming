2f3a994673178e7e042e1763c057a366588b77a5
efezino idisi <enduranceidisi13@gmail.com>
2022-10-16 23:58:24 +0100

    complete task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,45 @@
 #include "main.h"
 /**
+ * print2 - print two digit numbers
+ * @c: number
+ *
+ * Return: Nothing
+ */
+
+void print2(int c)
+{
+	if (c < 100)
+		_putchar(' ');
+	_putchar(c / 10 + '0');
+	_putchar(c % 10 + '0');
+}
+
+/**
+ * print3 - prints three digit numbers
+ * @c: number
+ *
+ * Return: Nothing
+ */
+
+void print3(int c)
+{
+	int x;
+
+	x = c % 10;
+	_putchar(c / 100 + '0');
+	c /= 10;
+	_putchar(c % 10 + '0');
+	_putchar(x + '0');
+}
+
+/**
  * print_times_table - prints the n times table
  * @n: the times table to be printed
  * Return: Nothing
  */
 void print_times_table(int n)
 {
-	int a, b, c, x;
+	int a, b, c;
 
 	if (n > 15 || n < 0)
 		return;
@@ -26,18 +59,11 @@ void print_times_table(int n)
 			}
 			else if (c > 9 && c < 100)
 			{
-				if (c < 100)
-					_putchar(' ');
-				_putchar(c / 10 + '0');
-				_putchar(c % 10 + '0');
+				print2(c);
 			}
 			else
 			{
-				x = c % 10;
-				_putchar(c / 100 + '0');
-				c /= 10;
-				_putchar(c % 10 + '0');
-				_putchar(x + '0');
+				print3(c);
 			}
 			if (a != (n + 1) && b != n)
 			{
