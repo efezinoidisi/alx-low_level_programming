be02f1df0030bfe1f7d9c8f5b5317f07686422d5
efezino idisi <enduranceidisi13@gmail.com>
2022-10-20 22:59:36 +0100

    add task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * execute_first - executes before main function
+ *
+ * Return: Nothing
+ */
+
+void execute_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
