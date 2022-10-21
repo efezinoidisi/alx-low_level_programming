0c8d489447a8fba0f6b85b927e642a005140a407
Idisi Efezino Endurance <enduranceidisi13@gmail.com>
2022-10-21 10:52:46 -0700

    add task 6

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+section .text:
+	default rel
+	extern printf
+	global main
+main:
+	push rbp
+	mov rdi, fmt
+	mov rsi, message
+	mov rax, 0
+	call printf wrt ..plt
+	pop rbp
+	mov rax, 0
+	ret
+
+section .data:
+	message: db "Hello, Holberton", 10, 0
+	fmt: db "%s", 0
