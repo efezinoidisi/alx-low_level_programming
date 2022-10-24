5312462c7c2dc2bb78b237a9cca3c321abbe6ee0
efezino idisi <enduranceidisi13@gmail.com>
2022-10-24 22:02:07 +0100

    update task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -32,6 +32,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp;
 	unsigned int len = listint_len(head) - 1, count;
 
+	if (head == NULL)
+		return (NULL);
+
 	if (index > len)
 	{
 		temp = NULL;
