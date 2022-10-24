e554bfbe4a89408d632e6938d6e0e1bde24ffceb
efezino idisi <enduranceidisi13@gmail.com>
2022-10-24 18:56:28 +0100

    add task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to first node
+ *
+ * Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	free(head);
+}
