3260f970f68f2d1ba9c294e2eb16b25f3566fab8
efezino idisi <enduranceidisi13@gmail.com>
2022-10-31 22:34:06 +0100

    remove library headers

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,4 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
  * read_textfile - reads a text file and prints it
