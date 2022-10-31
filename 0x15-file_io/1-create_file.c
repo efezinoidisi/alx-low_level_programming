be1c4a0603c1a9db257f53460f232850825c1bcc
efezino idisi <enduranceidisi13@gmail.com>
2022-10-31 22:38:06 +0100

    create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file
+ * @text_content: string to write to file
+ *
+ * Return: 1 on success or -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len;
+
+	fd = open(filename, O_WRONLY | O_CREAT, 0600);
+	if (fd == -1 || filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	len = str_len(text_content);
+	write(fd, text_content, len);
+	close(fd);
+	return (1);
+}
+
+/**
+ * str_len - returns the length of a string
+ * @s: string to be checked
+ *
+ * Return: length of string
+ */
+
+int str_len(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
