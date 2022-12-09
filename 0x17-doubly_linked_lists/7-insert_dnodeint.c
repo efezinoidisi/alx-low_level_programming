14124ee5517a483a71be8032ef34dc9c07b02a04
Idisi Efezino Endurance <enduranceidisi13@gmail.com>
2022-12-08 23:57:21 -0800

    func: add node at given index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - add new node at a given index
+ * @h: start of list
+ * @idx: index to add new node
+ * @n: data
+ *
+ * Return: address of new node or NULL
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *prev, *current, *new_node;
+	unsigned int i;
+
+	if (*h == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	current = *h;
+	i = 0;
+	while (current)
+	{
+		prev = current->prev;
+		if (i == idx)
+		{
+			prev->next = new_node;
+			new_node->prev = prev;
+			new_node->next = current;
+			current->prev = new_node;
+			break;
+		}
+		current = current->next;
+		i++;
+	}
+	return (new_node);
+}
