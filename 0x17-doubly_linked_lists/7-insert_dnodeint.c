66e0a23c2bad9ae3f5838abba4d399133994c840
Idisi Efezino Endurance <enduranceidisi13@gmail.com>
2022-12-09 01:11:14 -0800

    fix: add node at end

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,6 +26,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				new_node = add_dnodeint(h, n);
 				return (new_node);
 			}
+			else if (i == idx)
+			{
+				new_node = add_dnodeint_end(h, n);
+				return (new_node);
+			}
 			else
 			{
 				new_node = malloc(sizeof(dlistint_t));
