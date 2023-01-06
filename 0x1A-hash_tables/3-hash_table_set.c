934d16ae7babeb1a9a5222bee9a7eab320b08d38
efezino idisi <enduranceidisi13@gmail.com>
2023-01-07 00:27:18 +0100

    update function to update value if key already exits at the given index

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,12 +1,37 @@
 #include "hash_tables.h"
 
 /**
+ * check_key - check if key already exists
+ * @ht: hash table
+ * @key: key
+ * @index: index of array
+ *
+ * Return: 1 on success or 0 otherwise
+ */
+
+unsigned int check_key(hash_table_t *ht, const char *key, unsigned int index)
+{
+	hash_node_t *temp;
+
+	temp = ht->array[index];
+	while (temp)
+	{
+		if (temp->key == key)
+			return (1);
+		temp = temp->next;
+	}
+
+	return (0);
+}
+
+
+/**
  * hash_table_set - adds an element to the hash table
  * @ht: hash table
  * @key: key
  * @value: value associated with key
  *
- * Return: 1 on success and 0 otherwise
+ * Return: 1 on success or 0 otherwise
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
@@ -15,7 +40,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *str;
 	hash_node_t *node;
 
-	if (key == NULL || key[0] == '\0')
+	if (ht == NULL || key == NULL || key[0] == '\0')
 		return (0);
 	idx = key_index((const unsigned char *)key, ht->size);
 	str = strdup(value);
@@ -32,8 +57,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
-		node->next = ht->array[idx];
-		ht->array[idx] = node;
+		if (check_key(ht, key, idx))
+		{
+			ht->array[idx]->value = str;
+			free(node);
+		}
+		else
+		{
+			node->next = ht->array[idx];
+			ht->array[idx] = node;
+		}
 	}
 
 	return (1);
