2f759237f2de29e6a481c562d2e94d9522b9de0c
efezino idisi <enduranceidisi13@gmail.com>
2023-01-07 01:50:02 +0100

    add task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ *
+ * Return: Nothing
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *temp, *next;
+	int i;
+
+	if (ht == NULL)
+		return;
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return;
+	}
+	for (i = 0; i < (int)ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			temp = ht->array[i];
+			while (temp)
+			{
+				next = temp->next;
+				free(temp->key);
+				free(temp->value);
+				free(temp);
+				temp = next;
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
