a44a0ee82a4a9f0a95cb1974c00ef636a9b1e448
efezino idisi <enduranceidisi13@gmail.com>
2023-04-17 02:40:39 +0100

    fix typo

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -32,7 +32,7 @@ int interpolation_search(int *array, size_t size, int value)
 	{
 		pos = low + (((double)(high - low) / (array[high] - array[low]))
 			     * (value - array[low]));
-		printf("Valued checked array[%lu] = [%d]\n", pos, array[pos]);
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 		if (array[pos] < value)
 			low = pos + 1;
 		else if (array[pos] > value)
